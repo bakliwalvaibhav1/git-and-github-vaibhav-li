b6f6927d803837c3093626fdb18ef90d15d974f3
overclock144 <overclock144@gmail.com>
2023-09-18 13:58:45 -0500

    change bottle

--- bottles.py
+++ bottles.py
@@ -14,3 +14,5 @@ print(count, "bottles of", beverage)
 print("Take one down, pass it around")
 count = count - 1
 print(count, "bottles of", beverage, "on the wall")
+
+print("No more bottles of beer on the wall, no more bottles of beer.")
