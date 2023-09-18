2b3f0b83d8865f88fd622897ce2e09a83be86df7
Vaibhav Bakliwal <vaibhavbakliwal1998@gmail.com>
2023-09-18 14:15:23 -0500

    added alternate line logic

--- bottles.py
+++ bottles.py
@@ -1,13 +1,16 @@
 beverage = "beer"
 
 for count in range(100):
-
+    
     if count == 94:
         print("If one of those bottles should happen to fall, 98 bottles of beer on the wall...")
     else:
         print(count, "bottles of", beverage, "on the wall")
         print(count, "bottles of", "water")
-        print("Take one down, pass it around")
+        if count%2 == 0:
+            print("Take one down, pass it around")
+        else:
+            print("if one of those bottles should happen to fall")
         count = count - 1
         print(count, "bottles of", beverage, "on the wall")
 
