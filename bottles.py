18a954660cf2fd2372cae334f57311bb090972bb
Vaibhav Bakliwal <vaibhavbakliwal1998@gmail.com>
2023-09-18 14:00:37 -0500

    Added 5 bottles

--- bottles.py
+++ bottles.py
@@ -1,8 +1,32 @@
-beverage = "water"
-count = 2
+beverage = "beer"
+count = 5
 
 print(count, "bottles of", beverage, "on the wall")
 print(count, "bottles of", "water")
+print("Take one down, pass it around")
+count = count - 1
+print(count, "bottles of", beverage, "on the wall")
+
+print("")
+
+print(count, "bottles of", beverage, "on the wall")
+print(count, "bottles of", beverage)
+print("Take one down, pass it around")
+count = count - 1
+print(count, "bottles of", beverage, "on the wall")
+
+print("")
+
+print(count, "bottles of", beverage, "on the wall")
+print(count, "bottles of", beverage)
+print("Take one down, pass it around")
+count = count - 1
+print(count, "bottles of", beverage, "on the wall")
+
+print("")
+
+print(count, "bottles of", beverage, "on the wall")
+print(count, "bottles of", beverage)
 print("Take one down, pass it around")
 count = count - 1
 print(count, "bottles of", beverage, "on the wall")
