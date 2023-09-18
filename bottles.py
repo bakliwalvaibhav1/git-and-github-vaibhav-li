7b046fc8503d4776ca46955d79108daa7251e9ee
Vaibhav Bakliwal <vaibhavbakliwal1998@gmail.com>
2023-09-18 14:10:23 -0500

    added if condition for 6 to last verse

--- bottles.py
+++ bottles.py
@@ -1,12 +1,17 @@
 beverage = "beer"
 
 for count in range(100):
-    print(count, "bottles of", beverage, "on the wall")
-    print(count, "bottles of", "water")
-    print("Take one down, pass it around")
-    count = count - 1
-    print(count, "bottles of", beverage, "on the wall")
+
+    if count == 94:
+        print("If one of those bottles should happen to fall, 98 bottles of beer on the wall...")
+    else:
+        print(count, "bottles of", beverage, "on the wall")
+        print(count, "bottles of", "water")
+        print("Take one down, pass it around")
+        count = count - 1
+        print(count, "bottles of", beverage, "on the wall")
 
     print("")
 
 print("No more bottles of beer on the wall, no more bottles of beer.")
+print("We've taken them down and passed them around; now we're drunk and passed out!")
